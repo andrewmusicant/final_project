378ed8664a6f7b79ee3c638d136b29f6279f1690
Andrew Musicant <Andrewmusicant@yahoo.com>
2016-09-10 14:22:25 -0400

    Added the models

--- lending_app/models.py
+++ lending_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class State(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Bank(models.Model):
+    state = models.ForeignKey(State)
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=200)
+    net_income = models.IntegerField(default=0)
+    total_assets = models.IntegerField(default=0)
+    total_liability = models.IntegerField(default=0)
+    total_num_loans_small = models.IntegerField(default=0)
+    total_num_loans_micro = models.IntegerField(default=0)
+    total_amount_small = models.IntegerField(default=0)
+    total_amount_micro = models.IntegerField(default=0)
