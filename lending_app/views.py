3cc913dcdef417a8ab736ddc80069f7db7b3d482
Jon Alligood <j.alligood47@gmail.com>
2016-09-10 17:21:11 -0400

    Added base index and state html and view

--- lending_app/views.py
+++ lending_app/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render, HttpResponse
 
+from .models import Bank, State
+
 
 def index(request):
-    return HttpResponse('Hello World')
+    states = State.objects.all()
+    context = {
+        'states': states,
+    }
+    return render(request, 'index.html', context)
 
-# Create your views here.
+
+def state(request, pk):
+    pk = int(pk)
+    state = State.objects.get(pk=pk)
+    banks = Bank.objects.filter(state=state)
+    context = {
+        'banks': banks
+    }
+    return render(request, 'state.html', context)
