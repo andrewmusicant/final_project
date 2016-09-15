e4045ab99e160a537aba0d3410797db7749971a9
Jon Alligood <j.alligood47@gmail.com>
2016-09-15 08:54:46 -0400

    Updated data import to include more data

--- lending_app/migrations/0002_auto_20160910_1946.py
+++ lending_app/migrations/0002_auto_20160910_1946.py
@@ -9,7 +9,7 @@ import os
 
 
 def import_data(apps, schema_editor):
-    filename = os.path.abspath('filtered_bank_data.csv')
+    filename = os.path.abspath('bank_data.csv')
     Bank = apps.get_model("lending_app", "Bank")
     State = apps.get_model("lending_app", "State")
 
@@ -76,23 +76,32 @@ def import_data(apps, schema_editor):
         reader = csv.reader(f, delimiter=",")
         next(reader)
         for row in reader:
+            for index, item in enumerate(row):
+                if item == '':
+                    row[index] = 0
             name = row[0]
             location = row[1] + ", " + row[2] + ", " + row[3]  # city, state, zipcode
-            total_assets = row[6]
-            total_liability = row[7]
-            net_income = row[8]
-            if total_assets == '' or total_liability == '' or net_income == '':
-                print(name)
-            return_on_equity = int(net_income) / (int(total_assets) + int(total_liability))
-            total_loans = row[9]
-            total_amount_individual = row[10]
-            total_amount_micro = row[11]
-            total_amount_small = row[12]
+            number_of_employees = row[5] # +
+            total_assets = row[6] # +
+            total_liability = row[7] # +
+            total_deposits = row[8] # +
+            common_stock = row[9] # +
+            derivatives = row[10] # +
+            total_securities = row[11] # +
+            asset_backed_securities = row[12] # +
+            mortgage_backed_securities = row[13] # +
+            family_residential_loans = row[14] # +
+            home_equity_loans = row[15] # +
+            adjustable_rate_loans_secured_by_family_residential = row[16]
+            total_loans = row[17]
+            total_amount_individual = row[18]
+            total_amount_micro = row[19]
+            total_amount_small = row[21]
 
-            total_num_loans_micro = row[13]
-            total_num_loans_small = row[14]
-            total_amount_farm = row[15]
-            total_num_loans_farm = row[16]
+            total_num_loans_micro = row[20]
+            total_num_loans_small = row[22]
+            total_amount_farm = row[23]
+            total_num_loans_farm = row[24]
 
             if int(total_loans) == 0:
                 individual_loan_percentage = 0
@@ -105,12 +114,47 @@ def import_data(apps, schema_editor):
                 micro_loan_percentage = int(total_amount_micro) / int(total_loans)
                 farm_loan_percentage = int(total_amount_farm) / int(total_loans)
 
+            other_real_estate_owned = row[25]
+            real_estate_acquired = row[26]
+            other_real_estate_owned_family = row[27]
+            other_real_estate_owned_multi_family_residential = row[28]
+            other_real_estate_owned_commercial_real_estate = row[29]
+            other_real_estate_owned_construction_development = row[30]
+            other_real_estate_owned_farmland = row[31]
+            foreclosed_properties_from_GNMA_loans = row[32]
+            net_interest_margin = row[34] # + ?
+            noninterest_income_to_average_assets = row[35]
+            noninterest_expense_to_average_assets = row[36]
+            loan_and_lease_loss_provision_to_assets = row[37]
+            net_operating_income_to_assets = row[38]
+            return_on_assets = row[39]
+            return_on_equity = row[40]
+            retained_earnings_to_average_equity = row[41]
+            net_charge_offs_to_loans = row[42]
+            net_loans_and_leases_to_total_assets = row[43]
+            net_loans_and_leases_to_deposits = row[44]
+            net_loans_and_leases_to_core_deposits = row[45]
+            total_domestic_deposits_to_total_assets = row[46]
+            average_total_assets = row[47]
+            average_earning_assets = row[48]
+            average_equity = row[49]
+            average_total_loans = row[50]
+
             # Need to customize s for more states
             s = State.objects.get_or_create(name=states[row[2]])
             b = Bank(state=s[0], name=name, location=location,
-                     net_income=net_income, total_assets=total_assets,
+                     number_of_employees=number_of_employees,
+                     total_assets=total_assets,
                      total_liability=total_liability,
-                     return_on_equity=return_on_equity,
+                     total_deposits=total_deposits,
+                     common_stock=common_stock,
+                     derivatives=derivatives,
+                     total_securities=total_securities,
+                     asset_backed_securities=asset_backed_securities,
+                     mortgage_backed_securities=mortgage_backed_securities,
+                     family_residential_loans=family_residential_loans,
+                     home_equity_loans=home_equity_loans,
+                     adjustable_rate_loans_secured_by_family_residential=adjustable_rate_loans_secured_by_family_residential,
                      total_num_loans_small=total_num_loans_small,
                      total_num_loans_micro=total_num_loans_micro,
                      total_num_loans_farm=total_num_loans_farm,
@@ -120,7 +164,32 @@ def import_data(apps, schema_editor):
                      individual_loan_percentage=individual_loan_percentage,
                      small_loan_percentage=small_loan_percentage,
                      micro_loan_percentage=micro_loan_percentage,
-                     farm_loan_percentage=farm_loan_percentage)
+                     farm_loan_percentage=farm_loan_percentage,
+                     other_real_estate_owned=other_real_estate_owned,
+                     real_estate_acquired=real_estate_acquired,
+                     other_real_estate_owned_family=other_real_estate_owned_family,
+                     other_real_estate_owned_multi_family_residential=other_real_estate_owned_multi_family_residential,
+                     other_real_estate_owned_commercial_real_estate=other_real_estate_owned_commercial_real_estate,
+                     other_real_estate_owned_construction_development=other_real_estate_owned_construction_development,
+                     other_real_estate_owned_farmland=other_real_estate_owned_farmland,
+                     foreclosed_properties_from_GNMA_loans=foreclosed_properties_from_GNMA_loans,
+                     net_interest_margin=net_interest_margin,
+                     noninterest_income_to_average_assets=noninterest_income_to_average_assets,
+                     noninterest_expense_to_average_assets=noninterest_expense_to_average_assets,
+                     loan_and_lease_loss_provision_to_assets=loan_and_lease_loss_provision_to_assets,
+                     net_operating_income_to_assets=net_operating_income_to_assets,
+                     return_on_assets=return_on_assets,
+                     return_on_equity=return_on_equity,
+                     retained_earnings_to_average_equity=retained_earnings_to_average_equity,
+                     net_charge_offs_to_loans=net_charge_offs_to_loans,
+                     net_loans_and_leases_to_total_assets=net_loans_and_leases_to_total_assets,
+                     net_loans_and_leases_to_deposits=net_loans_and_leases_to_deposits,
+                     net_loans_and_leases_to_core_deposits=net_loans_and_leases_to_core_deposits,
+                     total_domestic_deposits_to_total_assets=total_domestic_deposits_to_total_assets,
+                     average_total_assets=average_total_assets,
+                     average_earning_assets=average_earning_assets,
+                     average_equity=average_equity,
+                     average_total_loans=average_total_loans)
             b.save()
         print("Banks imported")
 
