64e7a044ddca300b5c27ca0ce5b8d6f4c4f37c73
Jon Alligood <j.alligood47@gmail.com>
2016-09-21 11:49:01 -0400

    Linked bank detail to bank comparison

--- lending_app/urls.py
+++ lending_app/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
     url(r'^bank_detail/(?P<bank_id>[0-9]+)/$', views.bank_detail, name='bank_detail'),
     url(r'^location$', views.location, name='location'),
     url(r'^about$', views.about, name='about'),
-    url(r'^comparison/(?P<bank1_id>[0-9]+)&(?P<bank2_id>[0-9]+)/$', views.bank_comparison, name='bank_comparison'),
+    url(r'^comparison/(?P<bank1_id>[0-9]+)/(?P<bank2_id>[0-9]+)/$', views.bank_comparison, name='bank_comparison'),
 ]
