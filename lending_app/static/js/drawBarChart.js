f4c35fba7be102964b57ee338f5f13dc7da11170
Jon Alligood <j.alligood47@gmail.com>
2016-09-21 15:07:00 -0400

    Updated bank comparison with styles

---
+++ lending_app/static/js/drawBarChart.js
@@ -0,0 +1,48 @@
+drawChart('chart2');
+
+
+function drawChart(div) {
+  nv.addGraph(function() {
+    var chart = nv.models.discreteBarChart()
+        .x(function(d) { return d.label })    //Specify the data accessors.
+        .y(function(d) { return d.value })
+        .showYAxis(false)
+        .staggerLabels(false)    //Too many bars and not enough room? Try staggering labels.
+        .tooltips(false)        //Don't show tooltips
+        .showValues(true)       //...instead, show the bar value right on top of each bar.
+        .color(['#4DD0E1', '#0097A7', '#006064'])
+        ;
+
+    d3.select('#' + div + ' svg')
+        .datum(exampleData())
+        .transition().duration(350)
+        .call(chart);
+
+    nv.utils.windowResize(chart.update);
+
+    return chart;
+  });
+
+  //Each bar represents a single discrete quantity.
+  function exampleData() {
+   return  [
+      {
+        key: "Cumulative Return",
+        values: [
+          {
+            "label" : "Micro" ,
+            "value" : bankData.micro2
+          } ,
+          {
+            "label" : "Small" ,
+            "value" : bankData.small2
+          } ,
+          {
+            "label" : "Farm" ,
+            "value" : bankData.farm2
+          }
+        ]
+      }
+    ]
+  }
+}
